b1163af4cebafde5fc2fa26b95214c474b111df5
dmpstaltari <dmpstaltari@a8862cc9-0f1e-1f3a-7777-3084e61bc2e9>
2010-10-06 03:24:58 +0000

    Se agrega nuevo tipo de Cancelador.

--- Accionador/src/source/CanceladorPorDefecto.java
+++ Accionador/src/source/CanceladorPorDefecto.java
@@ -39,25 +39,6 @@ public class CanceladorPorDefecto extends Cancelador {
 		if(cancelacion) sucesosExistentes.remove(sucesoCancelable);
 				
 	}
-
-	private boolean seCancelan(Suceso suceso1, Suceso suceso2)
-	{
-		String idSuceso1 = suceso1.getIdSuceso();
-		String idSuceso2 = suceso2.getIdSuceso();
-		String idCanceladorSuceso1 = suceso1.getIdSucesoCancelador();
-		String idCanceladorSuceso2 = suceso2.getIdSucesoCancelador();
-		
-		if(idCanceladorSuceso1==null && idCanceladorSuceso2==null)
-			return false;
-		
-		if(idCanceladorSuceso1!=null && idCanceladorSuceso1.equals(idSuceso2))
-			return true;
-		
-		if(idCanceladorSuceso2!=null && idCanceladorSuceso2.equals(idSuceso1))
-			return true;
-		
-		return false;
-	}
 			
 
 }
--- Accionador/src/source/CanceladorTotal.java
+++ Accionador/src/source/CanceladorTotal.java
@@ -1,5 +1,7 @@
 package source;
 
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 public class CanceladorTotal extends Cancelador {
@@ -14,15 +16,64 @@ public class CanceladorTotal extends Cancelador {
 
 	@Override
 	public void cancelarSucesos(List<Suceso> sucesosExistentes,	List<Suceso> sucesosNuevos) {
-		
+		for(Suceso sucesoActual: sucesosNuevos)
+		{
+			cancelarSuceso(sucesosExistentes, sucesoActual);
+		}	
 	}
 
+	
 	@Override
 	public void cancelarSuceso(List<Suceso> sucesosExistentes,Suceso sucesoNuevo) {
 		
+		Iterator<Suceso> it = sucesosExistentes.iterator();
+		Suceso sucesoCancelable = null;
+		boolean cancelacion = false;
+		List<Suceso> listaSucesosCancelables = new ArrayList<Suceso>();
 		
+		while(it.hasNext()){
+			sucesoCancelable = it.next();
+			if(seCancelan(sucesoCancelable, sucesoNuevo)){
+				listaSucesosCancelables.add(sucesoCancelable);
+				cancelacion = true; 
+			}
+		}
+		if(cancelacion){
+			List<Suceso> listaAux = new ArrayList<Suceso>();
+			listaAux.addAll(sucesosExistentes);
+			listaAux.retainAll(listaSucesosCancelables);
+			sucesosExistentes.removeAll(listaAux);
+		}
 	}
 	
 	
-
+	/*
+	@Override
+	public void cancelarSuceso(List<Suceso> sucesosExistentes,Suceso sucesoNuevo) {
+		
+		Iterator<Suceso> it = sucesosExistentes.iterator();
+		Suceso sucesoCancelable = null;
+		boolean cancelacion = false;
+		
+		while(it.hasNext()&& !cancelacion)
+		{
+			sucesoCancelable = it.next();
+			cancelacion = seCancelan(sucesoCancelable, sucesoNuevo); 
+		}
+		
+		if(cancelacion){
+			
+			List<Suceso> listaSucesosCanselables = new ArrayList<Suceso>();
+			listaSucesosCanselables.add(sucesoCancelable);
+			
+			List<Suceso> listaAux = new ArrayList<Suceso>();
+			for(Suceso s: sucesosExistentes){
+				listaAux.add(s);
+			}
+						
+			listaAux.retainAll(listaSucesosCanselables);
+			sucesosExistentes.remove(listaAux);
+		}
+	}
+	*/
 }
