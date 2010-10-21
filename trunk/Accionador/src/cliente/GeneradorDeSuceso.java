21d8d5f27d1bd9a51d4ab5daea782de28fd9dff6
adrianachelotti <adrianachelotti@a8862cc9-0f1e-1f3a-7777-3084e61bc2e9>
2010-10-21 05:31:29 +0000

    Generador de Sucesos, ahora crea sucesos canceladores

--- trunk/Accionador/src/cliente/GeneradorDeSuceso.java
+++ trunk/Accionador/src/cliente/GeneradorDeSuceso.java
@@ -12,6 +12,10 @@ public class GeneradorDeSuceso {
 		String[] sucesosAGenerar = sucesoACrear.split(" ");
 		listaDeSucesosGeneradas = new ArrayList<Suceso>();
 		for (String identificadorActual : sucesosAGenerar) {
+			if (identificadorActual.contains(";")){
+				String[] identificadores =identificadorActual.split(";"); 
+				listaDeSucesosGeneradas.add(new Suceso(identificadores[0],identificadores[1]));
+			}else
 			listaDeSucesosGeneradas.add(new Suceso(identificadorActual));
 		}
 		return listaDeSucesosGeneradas;
