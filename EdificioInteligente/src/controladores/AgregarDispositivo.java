91805d3914797d1fe204f5b4971b6e65e1aeb771
adrianachelotti@gmail.com <adrianachelotti@gmail.com@a8862cc9-0f1e-1f3a-7777-3084e61bc2e9>
2010-11-18 06:05:42 +0000

    controlador para agregar dispositvo.

---
+++ EdificioInteligente/src/controladores/AgregarDispositivo.java
@@ -0,0 +1,39 @@
+package controladores;
+
+import java.util.List;
+import java.util.Map;
+
+import modelo.dispositivos.Dispositivo;
+import modelo.edificio.Edificio;
+
+import com.opensymphony.xwork2.Action;
+import com.opensymphony.xwork2.ActionContext;
+import com.opensymphony.xwork2.ActionSupport;
+
+import controladores.beans.EdificioBean;
+
+public class AgregarDispositivo extends ActionSupport {
+	private int nivel;
+	private Map<String,Object> session ;
+	
+	public void setSession(Map<String, Object> session) {
+		this.session = session;
+	}
+	public String execute(){
+		Map<String,Object> session = ActionContext.getContext().getSession();
+		Edificio edificio = Edificio.obtenerInstancia();
+		List<Dispositivo> dispositivos = edificio.getDriversDispositivos();
+		EdificioBean contenedor = new EdificioBean();
+		contenedor.setListaDeDisposititvos(dispositivos);
+		session.put("edificio",contenedor);
+		setSession(session);
+		return SUCCESS;
+	}
+	public int getNivel() {
+		return nivel;
+	}
+	public void setNivel(int nivel) {
+		this.nivel = nivel;
+	}
+
+}
