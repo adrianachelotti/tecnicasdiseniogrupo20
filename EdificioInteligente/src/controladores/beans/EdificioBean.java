038eb3d06018a385960c6d6f4b5889c5f2000dbf
adrianachelotti@gmail.com <adrianachelotti@gmail.com@a8862cc9-0f1e-1f3a-7777-3084e61bc2e9>
2010-11-18 06:09:46 +0000

    Clases contenedoras para pasar info entre el modelo y la vista

---
+++ EdificioInteligente/src/controladores/beans/EdificioBean.java
@@ -0,0 +1,31 @@
+package controladores.beans;
+
+import java.util.List;
+
+import modelo.dispositivos.Dispositivo;
+import modelo.edificio.Piso;
+
+public class EdificioBean {
+	
+	List<Piso> listadoDePisos;
+	
+	List<Dispositivo> listaDeDisposititvos;
+
+	public List<Dispositivo> getListaDeDisposititvos() {
+		return listaDeDisposititvos;
+	}
+
+	public void setListaDeDisposititvos(List<Dispositivo> listaDeDisposititvos) {
+		this.listaDeDisposititvos = listaDeDisposititvos;
+	}
+
+	public List<Piso> getListadoDePisos() {
+		return listadoDePisos;
+	}
+
+	public void setListadoDePisos(List<Piso> listadoDePisos) {
+		this.listadoDePisos = listadoDePisos;
+	}
+	
+
+}
