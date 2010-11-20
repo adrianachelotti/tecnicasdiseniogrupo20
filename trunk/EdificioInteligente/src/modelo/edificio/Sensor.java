5664bb0631e39592356f39beafaa8683d85d5650
adrianachelotti@gmail.com <adrianachelotti@gmail.com@a8862cc9-0f1e-1f3a-7777-3084e61bc2e9>
2010-11-20 20:18:46 +0000

    unificando la nomenclatura para los metodos

--- trunk/EdificioInteligente/src/modelo/edificio/Sensor.java
+++ trunk/EdificioInteligente/src/modelo/edificio/Sensor.java
@@ -4,54 +4,98 @@ import modelo.driver.DriverSensor;
 import modelo.manejadorDeSucesos.Suceso;
 
 public class Sensor{
+	
 	private Ubicacion ubicacion;
+	
 	private String descripcion;
+	
 	private DriverSensor driver;
 	
+	/**
+	 * Constructor del sensor cuyo driver es pasado como parametro
+	 * @param driver driver del sensor
+	 */
 	public Sensor(DriverSensor driver){
 		this.driver = driver;
 	}
 	
+	/**
+	 * Obtiene la medicion actual de sensor
+	 * @return  medicion del sensor
+	 */
 	public String obtenerMedicion(){
 		return this.driver.obtenerMedicion();
 	}
-	
+	/**
+	 * Establece la medicion del sensor
+	 * @param medicion medicion del sensor a cambiar
+	 */
 	public void establecerMedicion(String medicion){
 		this.driver.establecerMedicion(medicion);
 	}
-	
+	/**
+	 * Obtiene un suceso de acuerdo con la medicion actual
+	 * @return un suceso
+	 */
 	public Suceso notificarSuceso(){
 		return new Suceso(obtenerMedicion());
 	}
-		
+	/**
+	 * Habilita el sensor 
+	 */
 	public void habilitar(){
 		this.driver.encender();
 	}
-	
+	/**
+	 * Deshabilita el sensor
+	 */
 	public void deshabilitar(){
 		this.driver.apagar();
 	}
-		
+	/**
+	 * Obtiene si el sensor esta habilitado o no
+	 * @return true si esta habilitado
+	 * 		false si esta deshabilitado
+	 */
 	public boolean estaHabilitado() {
 		return this.driver.isEncendido();
 	}
 
-	public String getDescripcion() {
+	/**
+	 * Obtiene la descripcion del sensor
+	 * @return descripcion del sensor
+	 */
+	public String obtenerDescripcion() {
 		return descripcion;
 	}
-
-	public void setDescripcion(String descripcion) {
+	/**
+	 * Establece la descripcion para el sensor
+	 * @param descripcion descripcion del sensor
+	 */
+	public void establecerDescripcion(String descripcion) {
 		this.descripcion = descripcion;
 	}
-
-	public void setDriver(DriverSensor driver) {
+	
+	/**
+	 * Establece al sensor el driver pasado como parametro
+	 * @param driver del sensor
+	 */
+	public void establecerDriver(DriverSensor driver) {
 		this.driver = driver;
 	}
 
-	public Ubicacion getUbicacion() {
+	/**
+	 * Obtiene la ubicacion del sensor
+	 * @return la ubicacion del sensor
+	 */
+	public Ubicacion obtenerUbicacion() {
 		return ubicacion;
 	}
 	
+	/**
+	 * Establecer la ubicacion del sensor
+	 * @param ubicacion ubicacion del dispositivo
+	 */
 	public void setUbicacion(Ubicacion ubicacion) {
 		this.ubicacion = ubicacion;
 	}
