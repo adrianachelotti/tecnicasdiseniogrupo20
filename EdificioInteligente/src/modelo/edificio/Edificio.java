e1f227b2be9b549e220baa29a9607abdde10c180
dmpstaltari <dmpstaltari@a8862cc9-0f1e-1f3a-7777-3084e61bc2e9>
2010-11-20 11:12:46 +0000

    Se agrega habilitacion y deshabilitacion de reglas Se agrega identificador a las implicaciones

--- EdificioInteligente/src/modelo/edificio/Edificio.java
+++ EdificioInteligente/src/modelo/edificio/Edificio.java
@@ -1,10 +1,12 @@
 package modelo.edificio;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 import modelo.driver.DriverDispositivo;
 import modelo.driver.DriverSensor;
+import modelo.manejadorDeSucesos.Implicacion;
 
 
 public class Edificio {
@@ -51,6 +53,16 @@ public class Edificio {
 	}
 
 	/**
+	 * Obtiene el piso del nivel pasado como parametro.
+	 * @param nivel nivel del piso a buscar.
+	 * @return piso del edificio con el nivel pasado como parametro.
+	 */
+	public Piso obtenerPiso(int nivel){
+		int posicionPiso = this.getPisos().indexOf(new Piso(nivel));
+		return this.getPisos().get(posicionPiso);
+	}
+	
+	/**
 	 * Agrega el driver de un sensor al edificio.
 	 * @param sensor driver del sensor a agregar.
 	 */
@@ -82,5 +94,53 @@ public class Edificio {
 		return catalogoDriversDeDispositivos;
 	}
 	
+	/**
+	 * Obtiene las reglas que posee el edificio.
+	 * @return reglas que posee el edificio.
+	 */
+	public List<Implicacion> obtenerReglas(){
+		List<Implicacion> reglas=new ArrayList<Implicacion>();
+		for (Piso piso : pisos) {
+			reglas.addAll(piso.getManejadorDeSucesos().obtenerImplicaciones());
+		}
+		return reglas;
+	}
 	
+	/**
+	 * Obtiene la regla con el identificador pasado como parametro.
+	 * @param identificador identificador de la regla a buscar.
+ 	 * @return regla con el identificador.
+	 */
+	public Implicacion obtenerReglaPorIdentificador(int identificador){
+		boolean encontrado = false;
+		Iterator<Piso> it = pisos.iterator();
+		Implicacion implicacionActual=null;
+		Piso pisoActual = null;
+		while(it.hasNext() && !encontrado){
+			pisoActual=it.next();
+			implicacionActual = pisoActual.getManejadorDeSucesos().obtenerImplicacionPorIdentificador(identificador);
+			if(implicacionActual!=null){
+				encontrado=true;
+			}
+		}
+		return implicacionActual;
+	}
+	
+	/**
+	 * Habilita la regla cuyo identificador es el pasado como parametro.
+	 * @param identificador identificador de la regla a habilitar.
+	 */
+	public void habilitarRegla(int identificador){
+		Implicacion implicacion = obtenerReglaPorIdentificador(identificador);
+		if(implicacion!= null) implicacion.habilitar();
+	}
+	
+	/**
+	 * Deshabilita la regla cuyo identificador es el pasado como parametro.
+	 * @param identificador identificador de la regla a deshabilitar.
+	 */
+	public void deshabilitarRegla(int identificador){
+		Implicacion implicacion = obtenerReglaPorIdentificador(identificador);
+		if(implicacion!= null) implicacion.deshabilitar();
+	}
 }
