be28900b87e079254e25beb71fb12b4c9e100dbb
adrianachelotti@gmail.com <adrianachelotti@gmail.com@a8862cc9-0f1e-1f3a-7777-3084e61bc2e9>
2010-11-21 03:00:47 +0000

    Se agrega a la accion el nombre de la misma (para la vista)

--- trunk/EdificioInteligente/src/modelo/accion/AccionApagarDispositivo.java
+++ trunk/EdificioInteligente/src/modelo/accion/AccionApagarDispositivo.java
@@ -3,6 +3,10 @@ package modelo.accion;
 import modelo.manejadorDeSucesos.Accion;
 public class AccionApagarDispositivo extends Accion {
 
+	public AccionApagarDispositivo(){
+		this.establecerNombre("Apagar");
+	}
+	
 	@Override
 	public void ejecutar() {
 		this.obtenerDispositivo().apagar();
