2f2ea5ac0dca43f10ca6c6fcfd49a2d326d51933
adrianachelotti@gmail.com <adrianachelotti@gmail.com@a8862cc9-0f1e-1f3a-7777-3084e61bc2e9>
2010-11-24 05:21:00 +0000

    Comentando las clases

--- trunk/EdificioInteligente/src/modelo/driver/DriverDispositivo.java
+++ trunk/EdificioInteligente/src/modelo/driver/DriverDispositivo.java
@@ -1,12 +1,30 @@
 package modelo.driver;
-
+/**
+ * Interface que deben implementar los drivers de los dispositivos
+ * @author grupo20
+ *
+ */
 public interface DriverDispositivo {
 	
+	/**
+	 * Metodo para encender el dispositivo
+	 */
 	public void encender();
 	
+	/**
+	 * Metodo para apagar el dispositivo
+	 */
 	public void apagar();
 	
+	/**
+	 * Obtiene el nombre del dispositivo
+	 * @return nombre del dispositivo
+	 */
 	public String obtenerNombre();
 	
+	/**
+	 * Metodo que dice si esta encendido o no el dispositivo
+	 * @return verdadero si el dispositvo esta encendido, false caso contrario
+	 */
 	public boolean isEncendido();
 }
